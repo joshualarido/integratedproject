387d88b71a0d0b3cc0458339e93ed583e1ed6cf2
Joshua <joshualarido@gmail.com>
2024-03-18 22:31:39 +0700

    refined project page

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -5,20 +5,27 @@ var date = moment().format("dddd, Do MMMM, YYYY");
 const Game = () => {
     return (
         <div>
-            <div className="content-heading-container">
-                <img src="./src/assets/hotel-yamato.jpg" alt="hotel-yamato.jpg"
-                     className="absolute top-0 left-0 ml-64 header-img"/>
-                <div className="content-headings">
-                    <h3 className="content-heading">Projects / Construct 2 Game</h3>
-                    <h3 className="content-heading text-right">{date}</h3>
-                </div>
+            <div className="content-header">
+                    <div className="image-container">
+                        <img src="./src/assets/hotel-yamato.jpg" alt="hotel-yamato.jpg"
+                             className="top-0 left-0 header-img w-full h-full object-cover object-bottom"/>
+                        {/* <div className="overlay"></div> */}
+                    </div>
             </div>
-            <div>
-                <h1 className="page-header">Yamato Hotel Incident</h1>
-                <div className="flex place-content-center my-8">
-                    <div className="p-3 rounded-md h-[480px] w-[854px] bg-white shadow-lg"></div>
+            <div className="content-main">
+                <div className="content-heading-container">
+                    <div className="content-headings">
+                        <h3 className="content-heading">Projects / Construct 2 Game</h3>
+                        <h3 className="content-heading text-right">{date}</h3>
+                    </div>
                 </div>
-                <p className="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dolor animi aliquid maiores voluptas! Neque, asperiores dolor! Odit est alias vel eos fuga reprehenderit quae sit autem nemo! Officiis, consequatur incidunt, culpa explicabo eaque sequi exercitationem in facilis, laboriosam ea accusamus quisquam dolores nulla possimus magnam impedit. Nisi reiciendis nobis pariatur incidunt perspiciatis at eius nam ex aperiam cum beatae dolorum, ad velit delectus labore non nemo. Nihil quos excepturi assumenda, similique cum perspiciatis sint nobis iusto, eum cumque distinctio laborum exercitationem dolores modi. Nisi facilis inventore obcaecati ullam reiciendis nam deleniti tenetur voluptatibus, voluptatem ducimus velit tempora aliquid ipsum.</p>
+                <div>
+                    <h1 className="page-header">Yamato Hotel Incident</h1>
+                    <div className="flex place-content-center my-8">
+                        <div className="p-3 rounded-md h-[480px] w-[854px] bg-white shadow-lg"></div>
+                    </div>
+                    <p className="content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dolor animi aliquid maiores voluptas! Neque, asperiores dolor! Odit est alias vel eos fuga reprehenderit quae sit autem nemo! Officiis, consequatur incidunt, culpa explicabo eaque sequi exercitationem in facilis, laboriosam ea accusamus quisquam dolores nulla possimus magnam impedit. Nisi reiciendis nobis pariatur incidunt perspiciatis at eius nam ex aperiam cum beatae dolorum, ad velit delectus labore non nemo. Nihil quos excepturi assumenda, similique cum perspiciatis sint nobis iusto, eum cumque distinctio laborum exercitationem dolores modi. Nisi facilis inventore obcaecati ullam reiciendis nam deleniti tenetur voluptatibus, voluptatem ducimus velit tempora aliquid ipsum.</p>
+                </div>
             </div>
         </div>
     );
